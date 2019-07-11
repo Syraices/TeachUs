bf8d91a39a41dff370cf1d75b96b6fc2a02a57ea
Shmulik <syraices@gmail.com>
2019-07-11 03:54:45 +0000

    Finalizing permissions

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -1,26 +1,34 @@
 class Instructor::SectionsController < ApplicationController
-  def new
-    @course = Course.find(params[:course_id])
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_course
     # if @course.user != current_user
     #   render plain: 'Unauthorized', status: :Unauthorized
     # end
+  def new
     @section = Section.new
   end
 
   def create 
-    @course = Course.find(params[:course_id])
     @section = @course.sections.create(section_params)
     redirect_to instructor_course_path(@course)
   end
-end
+  
 
 
 private 
 
-# def current_section
-#   @current_section ||= Section.find(params[:section_id])
-# end
+def require_authorized_for_current_course
+  if current_course.user != current_user
+    render plain: 'Get the HELL out of here!!! You do NOT belong here!!!', status: :unauthorized
+  end
+end
 
-def section_params
-  params.require(:section).permit(:title)
+  helper_method :current_course
+  def current_course
+    @current_course ||= Course.find(params[:course_id])
+  end
+
+  def section_params
+    params.require(:section).permit(:title)
+  end
 end
