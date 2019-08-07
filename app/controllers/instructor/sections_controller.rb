25ed7a9157b157c17e131c3df8a10bd1ab7ca557
Shmulik <syraices@gmail.com>
2019-08-07 15:09:14 +0000

    Adding a static page

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -1,7 +1,7 @@
 class Instructor::SectionsController < ApplicationController
   before_action :authenticate_user!
   before_action :require_authorized_for_current_course, only: [:create]
-  
+  before_action :require_authorized_for_current_section, only: [:update]
 
   def create 
     @section = current_course.sections.create(section_params)
@@ -23,6 +23,12 @@ class Instructor::SectionsController < ApplicationController
 
   def require_authorized_for_current_course
     if current_course.user != current_user
+      render plain: 'Get the HELL out of here!!! You do NOT belong here!!!', status: :unauthorized
+    end
+  end
+
+    def require_authorized_for_current_section
+    if current_section.course.user != current_user
       render plain: 'Get the HELL out of here!!! You do NOT belong here!!!', status: :unauthorized
     end
   end
